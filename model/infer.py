bd0147737bd7764b7239999b340f45afde27327a
Twet-ta <rain.illusive@mail.ru>
2023-12-01 01:31:30 +0300

    new change

--- model/infer.py
+++ model/infer.py
@@ -1,35 +1,61 @@
+from pickle import load
+
+import dvc.api
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score
+from sklearn.model_selection import train_test_split
+
+
+def load_data():
+    # Load the dataset locally or through DVC
+    with dvc.api.open_url("model/iris_dataset.csv", repo="myremote") as fd:
+        df = pd.read_csv(fd)
+
+    X = df.drop("target", axis=1)
+    y = df["target"]
+    return X, y
+
+
+def load_model():
+    # Load the trained Support Vector Machine model using DVC
+    with dvc.api.open_url("model/iris_model.pkl", repo="myremote") as file:
+        model = load(file)
+    return model
+
+
+def save_predictions(X_test, y_test, predictions):
+    # Save predictions to results.csv
+    with open("./results.csv", "w") as file:
+        file.write("x, y_true, y_pred\n")
+        for X_i, y_i, y_pred_i in zip(X_test.values, y_test, predictions):
+            s = str(X_i) + "," + str(y_i) + "," + str(y_pred_i) + "\n"
+            file.write(s)
+
+
+def print_metrics(y_test, predictions):
+    # Print evaluation metrics
+    print("accuracy: ", accuracy_score(y_test, predictions))
+    print("f1: ", f1_score(y_test, predictions, average="micro"))
+    print("precision: ", precision_score(y_test, predictions, average="micro"))
+
+
 if __name__ == "__main__":
-    from pickle import load
-
-    from sklearn.datasets import load_iris
-    from sklearn.metrics import accuracy_score, f1_score, precision_score
-    from sklearn.model_selection import train_test_split
-
-    iris = load_iris()
-    X = iris["data"]
-    y = iris["target"]
-    names = iris["target_names"]
-    feature_names = iris["feature_names"]
+    # Load data
+    X, y = load_data()
 
     # Split the data set into training and testing
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=2
     )
 
-    # Support vector machine algorithm
-    with open("./iris_pickle.pkl", "rb") as file:
-        svn = load(file)
+    # Load model
+    trained_model = load_model()
 
     # Predict from the test dataset
-    predictions = svn.predict(X_test)
-    # Calculate the accuracy
+    predictions = trained_model.predict(X_test)
 
-    with open("./results.csv", "w") as file:
-        file.write("x, y_true, y_pred\n")
-        for X_i, y_i, y_pred_i in zip(X_test, y_test, predictions):
-            s = str(X_i) + "," + str(y_i) + "," + str(y_pred_i) + "\n"
-            file.write(s)
+    # Save predictions
+    save_predictions(X_test, y_test, predictions)
 
-    print("accuracy: ", accuracy_score(y_test, predictions))
-    print("f1: ", f1_score(y_test, predictions, average="micro"))
-    print("precision: ", precision_score(y_test, predictions, average="micro"))
+    # Print metrics
+    print_metrics(y_test, predictions)
--- model/train.py
+++ model/train.py
@@ -1,23 +1,45 @@
+from pickle import dump
+
+import dvc.api
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+
+def load_data():
+    data_dvc_path = "data/iris_dataset.csv"
+    remote_name = "myremote"
+    with dvc.api.open(data_dvc_path, repo=remote_name) as fd:
+        df = pd.read_csv(fd)
+
+    X = df.drop("target", axis=1)
+    y = df["target"]
+    return X, y
+
+
+def train_model(X_train, y_train):
+    svc = SVC()
+    svc.fit(X_train, y_train)
+    return svc
+
+
+def save_model(model):
+    # Save the trained model using DVC
+    with open("model/iris_model.pkl", "wb") as fd:
+        dump(model, fd)
+
+
 if __name__ == "__main__":
-    from pickle import dump
-
-    from sklearn.datasets import load_iris
-    from sklearn.model_selection import train_test_split
-    from sklearn.svm import SVC
-
-    iris = load_iris()
-    X = iris["data"]
-    y = iris["target"]
-    names = iris["target_names"]
-    feature_names = iris["feature_names"]
+    # Load data
+    X, y = load_data()
 
     # Split the data set into training and testing
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=2
     )
 
-    svn = SVC()
-    svn.fit(X_train, y_train)
+    # Train model
+    trained_model = train_model(X_train, y_train)
 
-    with open("./iris_pickle.pkl", "wb") as file:
-        dump(svn, file)
+    # Save model
+    save_model(trained_model)
